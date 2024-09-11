b73bee3e18686a364d27fe1aecb4713d497772da
just-makab <lwethusibisi@gmail.com>
2024-09-11 22:28:35 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROG7312_Ethekwini_Connect
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
